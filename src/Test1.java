124d170441f26181147cc2b3a3da63e6031f9fec
excelaiyt <excelaiyt12@gmail.com>
2023-10-11 20:26:38 +0300

    add commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+    public static void main(String[] args) {
+    }
+
+    public static void calcCircleRadius(double area) {
+
+        double Pi = 3.14;
+        area = 5;
+        System.out.print(2 * Pi * area);
+
+    }
+}
+
+
+
+
